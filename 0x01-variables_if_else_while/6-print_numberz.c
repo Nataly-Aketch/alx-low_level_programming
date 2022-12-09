96bbd3e5e221ee50de0a70e21f0bd5d19f14df59
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-09 05:04:19 -0800

    Prints 0 to 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9 while using
+ * putchar() function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
