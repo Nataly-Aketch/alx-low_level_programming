34ee6e0223ff698f61d4c913fe7c7cc69ee36207
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-09 01:28:03 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (l < 6 && !0)
+	else if (l < 6 && !(0))
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
