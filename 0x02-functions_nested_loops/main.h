079d363492cb055d81968b76616bfc1b767ff903
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-13 05:48:22 -0800

    Prints the lower alphabet 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,28 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+void print_alphabet(void)
+{
+	char A = 'a';
+	while (A <= 'z')
+	{
+		_putchar(A);
+		A++;
+	}
+	_putchar('\n');
+}
+void print_alphabet_x10(void)
+{
+	char n = 0;
+	while (n < 10)
+	{
+		char l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
