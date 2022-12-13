5a3c618aacc2fb5b27780cfee684582c2909aa5c
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-13 12:09:19 -0800

    Checks for the alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether a character is
+ * an alphabet
+ * @c: integer to be checked
+ *
+ * Return: 1 if it's an alphabet, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0)
+}
