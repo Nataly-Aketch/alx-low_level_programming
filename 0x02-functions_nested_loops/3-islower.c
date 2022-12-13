213e0e42d7c854f6fde46ab96a27752572d21b4d
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-13 10:36:49 -0800

    Checks for lowercase letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * main = check the code
+ * _islower - checks whether an integer c
+ * is a lowercase letter.
+ * @c: an integer value that is checked for
+ * lowercase or otherwise.
  *
- * Return: Always 0
+ * Return: 1 if integer is lowercase 0 if
+ * otherwise
  */
 
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
 
