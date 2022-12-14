6a2d9839c431abca4e8119cfdd131be65958f8f1
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-14 12:54:21 -0800

    Prints multliplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if ((n > 15) && (n < 0))
+	if ((n <= 15) && (n >= 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -36,6 +36,7 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				}
 			}
+			_putchar('\n');
 		}
 	}
 }
