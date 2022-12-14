2f39908b206410529d68c99b561e819304addaca
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-14 02:00:41 -0800

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 
 			if (j == 0)
 				_putchar(k + '0');
-			if (k < 10 && j != 0)
+			if (k <= 9 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -29,8 +29,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k + '0');
-				_putchar(k + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 	}
