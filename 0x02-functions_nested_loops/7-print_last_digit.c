7b09f262e3aa814ba157c888b91576afcfc2f839
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-14 00:42:17 -0800

    Prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,15 @@
 /**
  * print_last_digit - prints last digit of number using
  * _putchar.
- * @int: integer data type for variable.
+ * @i: integer data type for variable.
  *
  * Return: Value of last digit.
  */
 
 int print_last_digit(int i)
 {
-	return (i / 10);
+	int l = i % 10;
+
+	_putchar(l + '0');
+	return (i % 10);
 }
