07ab2a7bb2f175c20811e909c3559e036ca97024
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-13 19:19:10 -0800

    Prints n t0 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints values from 98
+ * and to 98.
+ *
+ * @n: integer to be checked
+ * Return: 0 success
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+}
