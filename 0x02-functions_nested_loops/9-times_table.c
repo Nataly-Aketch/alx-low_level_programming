d414886794311991256112de7060191e590010e5
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-13 22:16:03 -0800

    Prints x9 multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,15 +10,15 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
 
 			if (j == 0)
 				_putchar(k + '0');
-			if (k < 10 && j != 0)
+			if (k <= 9 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
