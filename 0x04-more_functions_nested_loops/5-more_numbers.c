60d996342280b1ff2240812147d0e564726ddc02
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-15 03:17:49 -0800

    Prints 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,12 @@ void more_numbers(void)
 
 		while (j <= 14)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar(j + '0');
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
