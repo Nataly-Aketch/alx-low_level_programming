b638ebfa2e22d8343b9cd6e83a8a7a47875daf80
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-15 05:24:56 -0800

    Prints lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines n number of times
+ * @n: number of times
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else if (n <= 0)
+		_putchar('\n');
+}
