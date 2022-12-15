33499ec43f25693860f00df55c09a0485a6889dd
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-15 08:44:09 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,17 @@ void fizzbuzz(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
+		if (i % 15 == 0)
 		{
-			if (i % 15 == 0)
-			{
-				printf(" FizzBuzz");
-			}
-			if (i % 3 == 0)
-			{
-				printf(" Fizz");
-			}
-			if (i % 5 == 0)
-			{
-				printf(" Buzz");
-			}
+			printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
 		}
 		else if (i == 1)
 		{
