2464380a27e087bfbbae3a36a38f483febb23213
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-15 10:31:21 -0800

    Prints diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,9 +11,9 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				if (j == i)
 					putchar('\\');
