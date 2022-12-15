c4283e3c4a57c75e62b73ebb8b673993315313a0
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-15 09:28:07 -0800

    Squares

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_diagonal - draws a diagonal line
  * @n: number of times
@@ -9,14 +9,27 @@ void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		int i;
+		int i, j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('\\');
+			for (j = 0; j <= n; j++)
+			{
+				if (j == i)
+					putchar('\\');
+				else if (j < i)
+					putchar(' ');
+			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
-		_putchar('\n');
+		putchar('\n');
+}
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(-4);
+	print_diagonal(2);
+	print_diagonal(10);
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints squares
+ * @size: number of times square is to be printed
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
