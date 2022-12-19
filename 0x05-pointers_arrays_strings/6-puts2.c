d2db755a97170240b2fb74c0a8118f2e83c2db04
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-19 09:19:48 -0800

    EOF

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts2 - prints every other character of a string
@@ -7,10 +8,10 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	while (*str != ('\0' || EOF))
 	{
-		_putchar(*str);
+		putchar(*str);
 		str += 2;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
