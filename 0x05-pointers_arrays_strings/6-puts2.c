97e2bae0326f1825163205c201dd57bd1a1ec6af
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-19 09:53:30 -0800

    EOF

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,12 @@
 
 void puts2(char *str)
 {
-	if (*str != EOF)
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		while (*str != '\0')
-		{
-			_putchar(*str);
-			str += 2;
-		}
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
