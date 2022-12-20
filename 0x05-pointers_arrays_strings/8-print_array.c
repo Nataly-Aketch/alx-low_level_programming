981ca585d702240a6268b3b784468f17b1d4cb6e
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-19 18:47:37 -0800

    Prints array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,17 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int len = sizeof(a) / sizeof(a[0]);
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < n; i++)
 	{
+		if (i != 0)
+			printf(", ");
 		printf("%d", a[i]);
 	}
 	printf("\n");
+}
+int main()
+{
+	int arr[5] = {1, 2, 3, 4, 5};
+	print_array(arr, 5);
 }
