ec70843812d52f1c028878ec254a2d47dfe86abd
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-19 20:00:41 -0800

    Random passwd generatot

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,14 +10,21 @@
 int password_gen(int len)
 {
 	char list[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-`~,./';[]\"\\{}:><?";
+	int i;
+
 	srand(time(NULL));
-	for (int i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
-		printf("%c", list[rand() % (sizeof list - 1)]);
+		printf("%c", list[rand() % (sizeof(list) - 1)]);
 	}
 	printf("\n");
 }
+/**
+ *main - checks code
+ * Return: Always 0
+ */
 int main(void)
 {
 	password_gen(15);
+	return (0);
 }
