1dd5de416cfa9f5e08c91b392ae5b44b72c72412
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-22 17:35:13 +0300

    Cocatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
-	/*till it doesn't point to a NULL*/
+	int i;
+	int j;
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	j = 0;
 
+	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	*dest = '\0';
+
+	dest[i] = '\0';
 	return (dest);
+
 }
