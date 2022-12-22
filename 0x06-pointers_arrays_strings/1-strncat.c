98bb565921f9a4dae5a4f389b35c0fc1ca6d4d85
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-22 18:01:23 +0300

    Concatenate n bytes of a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,20 +12,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-	}
-
-	j = 0;
-
-	while (j < n && src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
 		i++;
 	}
-	dest = '\0';
+
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
