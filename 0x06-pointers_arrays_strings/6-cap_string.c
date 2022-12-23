339b9adab4d11af39f98f5517bd6fcfd3b40e0b3
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-23 23:17:28 +0300

    Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes string
+ * @a: string
+ * Return: pointer to a
+ */
+
+char *cap_string(char *a)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < i)
+	{
+		if (j == 0 && s[j] >= 'a' && s[j] <= 'z')
+		{
+			s[j] = s[j] - 32;
+		}
+
+		if ((s[j] == ',' || s[j] == ';' || s[j] == '.'
+					|| s[j] == '!' || s[j] == '?' || s[j] == '"'
+					|| s[j] == '(' || s[j] == ')' || s[j] == '{'
+					|| s[j] == '}' || s[j] == ' ' || s[j] == '\t'
+					|| s[j] == '\n') && (s[j + 1] >= 'a' && s[j + 1] <= 'z'))
+		{
+			s[j + 1] = s[j + 1] - 32;
+		}
+		j++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *a);
