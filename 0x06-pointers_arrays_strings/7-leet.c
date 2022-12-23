ee7b63e824926f5e03462ef7f2d0e20eaea1c6ac
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-24 00:28:02 +0300

    Encodes 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,11 +9,11 @@
 
 char *leet(char *s)
 {
-	int i, j;
+	int i = 0, j;
 	char str[] = "aAeEoOtTlL";
 	char str1[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
