68902b659916f000656b428d15f496cc9d7fdbbb
Nataly-Aketch <nataliebeckie@gmail.com>
2022-12-31 00:36:57 +0300

    Prints number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,14 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	if (n1 / 10 != 0)
+		print_number(n1 / 10);
+	_putchar(n1 % 10 + '0');
 
 }
