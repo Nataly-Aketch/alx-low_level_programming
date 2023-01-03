1942dd3a2a52ef32781872994a52793a44751e88
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-03 14:51:26 +0300

    copies meory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * _memcpy - copies memory area
  * @dest: destination to be copied to
  * @src: source to be copied from
+ * @n: number of times
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
