a87587667b9841b67404d31bf6f3a22845504c1c
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-03 14:42:54 +0300

    copies meory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination to be copied to
+ * @src: source to be copied from
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
