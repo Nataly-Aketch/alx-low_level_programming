a4b1cbbf1be4596a8ab051b147ce28d979b5f3fa
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-06 21:27:43 +0300

    Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,26 +8,12 @@
  */
 int is_prime_number(int n)
 {
-}
-int main(void)
-{
-	    int r;
-
-	        r = is_prime_number(1);
-		    printf("%d\n", r);
-		        r = is_prime_number(1024);
-			    printf("%d\n", r);
-			        r = is_prime_number(16);
-				    printf("%d\n", r);
-				        r = is_prime_number(17);
-					    printf("%d\n", r);
-					        r = is_prime_number(25);
-						    printf("%d\n", r);
-						        r = is_prime_number(-1);
-							    printf("%d\n", r);
-							        r = is_prime_number(113);
-								    printf("%d\n", r);
-								        r = is_prime_number(7919);
-									    printf("%d\n", r);
-									        return (0);
+	if (n == 2)
+		return (1);
+	if (n < 2)
+		return (0);
+	if (n % 2 == 0 || is_prime_number(n - 1))
+		return (0);
+	else
+		return (1);
 }
