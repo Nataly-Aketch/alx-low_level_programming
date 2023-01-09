e2446a4259ee312d1646900107eb501eac420fae
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-09 22:27:36 +0300

    Adds CLI arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,12 +10,19 @@
  */
 int main(int argc, char **argv)
 {
-	int i = 0, sum = 0;
-	
+	int i = 1, sum = 0;
+
 	for (; i < argc; i++)
 	{
 		sum += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	if ((strcmp(argv[argc - 1], "1") == 0) || (strcmp(argv[argc - 1], "2") == 0)
+	|| (strcmp(argv[argc - 1], "3") == 0) || (strcmp(argv[argc - 1], "4") == 0) ||
+	(strcmp(argv[argc - 1], "5") == 0) || (strcmp(argv[argc - 1], "6") == 0) ||
+	(strcmp(argv[argc - 1], "7") == 0) || (strcmp(argv[argc - 1], "8") == 0) ||
+	(strcmp(argv[argc - 1], "9") == 0))
+		printf("%d\n", sum);
+	else
+		printf("Error\n");
 	return (0);
 }
