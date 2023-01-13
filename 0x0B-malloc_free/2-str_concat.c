525b269806bef39363ce22d199f22878c41f3e15
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-13 13:08:58 +0300

    Concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, n, i;
 	char *temp;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	n = len1 + len2 + 1;
