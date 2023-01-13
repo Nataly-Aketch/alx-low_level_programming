886cec471e4ea9b26c32a2b3c27e56a42cf806d7
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-13 13:36:16 +0300

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (0);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (0);
