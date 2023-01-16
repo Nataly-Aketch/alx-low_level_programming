0fe0f3ba30f19f03f155e04e6195f714b9a547c6
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-16 22:43:40 +0300

    String row

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to be split
+ * Return: Split string
+ */
+char **strtow(char *str)
+{
+	int i, j, col, row, total;
+	char **temp;
+
+	if (str == NULL || strcmp(str, "") == 0)
+		return (NULL);
+	total = sizeof(temp);
+	col = sizeof(temp[0]);
+	row = total / col;
+	temp = malloc(row * sizeof(char));
+	for (i = 0; i < total; i++)
+	{
+		temp[i] = malloc(col * sizeof(char));
+	}
+	return (temp);
+}
