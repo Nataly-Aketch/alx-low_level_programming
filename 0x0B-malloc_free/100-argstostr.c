9a461503dad40d41dd612ac577c8cc16acd5838c
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-16 22:13:47 +0300

    CLI arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,16 +30,12 @@ char *_strcat(char *dest, char *src)
 char *argstostr(int ac, char **av)
 {
 	char *temp;
-	int i, j, len;
+	int i, total;
 
 	if (ac == 0 || av == NULL)
 		return (0);
-	for (i = 1; av[i]; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-	}
-	temp = malloc(len * sizeof(char));
+	total = sizeof(av);
+	temp = malloc(total * sizeof(char));
 	if (temp == NULL)
 		return (0);
 	for (i = 0; i < ac; i++)
