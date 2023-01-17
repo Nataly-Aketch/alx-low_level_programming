2069b4db8ee3a7bd3d165be04d3c37cfd6b1e4ae
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-17 12:29:14 +0300

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+#define ABS(x) ((x < 0) ? -(x) : (x))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
 #define SUM(x, y) ((x) + (y))
 #endif
