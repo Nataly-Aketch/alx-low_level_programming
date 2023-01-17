8fa05deac849773e9b5776363b1e3185e1210c00
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-17 17:46:53 +0300

    Structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
