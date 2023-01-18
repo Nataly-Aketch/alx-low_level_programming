6e928e13bdf7e1fd092d120b64962b8c8550e652
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-18 17:51:20 +0300

    Free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,6 +20,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy->name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (doggy->name == NULL)
 	{
+		free(doggy);
 		return (0);
 	}
 	strcpy(doggy->name, name);
