8749d336ae6dc9e0d6e5e3972af4ef21cdcca4fe
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-19 14:17:26 +0300

    Prints numbers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,24 @@
  * @array: array to be searched
  * @size: array length
  * @cmp: function pointer to be used to compare values
+ * Return: index of first element for which cmp does
+ * not return 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (size <= 0)
-		return -1;
+		return (-1);
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
-				return (i);
+			{
+				if (i)
+					return (i);
+			}
 		}
 	}
 	return (0);
