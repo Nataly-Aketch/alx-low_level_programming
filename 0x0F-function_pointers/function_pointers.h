2a89d3b32c970ca31734be1a4db3764f36d11db7
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-19 13:36:46 +0300

    Callback function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
