9d0d44cf5b928b84ebd6361c7e5bcb4b9e77f22c
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-19 13:36:33 +0300

    Callback function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * array_iterator - callback function
+ * @array: array
+ * @size: array size
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = array[0]; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
