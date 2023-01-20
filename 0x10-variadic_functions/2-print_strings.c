e028811fff6295bec5168ffa9abf22eac4bb5823
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-20 17:20:11 +0300

    Print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i != n - 1)
+		if (i != n - 1 && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
