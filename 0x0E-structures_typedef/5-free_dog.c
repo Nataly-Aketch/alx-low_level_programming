04d8985ba5cf5d3edda097196f84fe45d97fa7fe
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-22 23:44:34 +0300

    Free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  * free_dog - frees dog
- * @d: struct to be freed
+ * @d:struct to be freed
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
 		free(d->name);
 		free(d->owner);
