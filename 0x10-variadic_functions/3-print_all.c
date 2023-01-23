7a9c823a83472ecb75d0ddf14928b704ad30c33c
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-23 13:52:13 +0300

    Print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,33 +14,29 @@ void print_all(const char * const format, ...)
 	va_start(ap, format);
 	while (j < len)
 	{
+		if (j < 4 && j > 0)
+			printf(", ");
 		switch (format[j])
 		{
 			case ('i'):
-			{
 				printf("%d", va_arg(ap, int));
 				break;
-			}
 			case ('c'):
-			{
 				printf("%c", va_arg(ap, int));
 				break;
-			}
 			case ('f'):
-			{
 				printf("%f", va_arg(ap, double));
 				break;
-			}
 			case ('s'):
-			{
-				if (!string)
-					printf("(nil)");
 				string = va_arg(ap, char *);
+				if (!string)
+				{
+					printf("(nil)");
+					break;
+				}
 				printf("%s", string);
-			}
+				break;
 		}
-		if (j != len - 1)
-			printf(", ");
 		j++;
 	}
 	printf("\n");
