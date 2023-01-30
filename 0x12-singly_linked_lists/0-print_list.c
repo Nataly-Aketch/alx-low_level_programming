c4db56b86ea51efa72f436360e27deb66d8348d0
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-30 13:31:57 +0300

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a linked list
+ * @h: pointer to head node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
