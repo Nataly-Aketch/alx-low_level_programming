e3005fb975b7963280bb3d7eaecd69b87abaa423
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-30 15:16:54 +0300

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,21 +9,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *dup;
+	/*char *dup;*/
 	int length = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (0);
-	dup = strdup(str);
-	if (!dup)
+	new_node->str = strdup(str);
+	if (!str)
 	{
 		free(new_node);
 		return (0);
 	}
 	while (str[length])
 		length++;
-	new_node->str = dup;
 	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
