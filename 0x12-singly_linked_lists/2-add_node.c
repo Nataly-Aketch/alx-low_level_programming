d95dec0f10c17dec35ae6299778635d64bb37a38
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-30 14:49:27 +0300

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (0);
 	new_node->str = strdup(str);
+	if (!new_node->str)
+		return (0);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
