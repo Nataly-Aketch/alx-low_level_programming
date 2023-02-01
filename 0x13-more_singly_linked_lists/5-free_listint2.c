120ad14537440030f7748a5f9d5114441b459bdf
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-01 21:39:33 +0300

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list and sets head to null
+ * @head: head node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+}
