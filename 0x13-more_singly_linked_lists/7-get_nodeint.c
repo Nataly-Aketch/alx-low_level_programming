f65ade977758f4df80cebd8af039476cb91bd361
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-01 23:03:52 +0300

    Search node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - searches a list
+ * @head: head node
+ * @index: node index
+ * Return: index of the node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head)
+	{
+		head = head->next;
+		count++;
+		if (count == index)
+			return (head);
+	}
+	return (0);
+}
