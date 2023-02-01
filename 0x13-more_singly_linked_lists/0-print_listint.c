d4724536042e7caf3d02abe1ebecbc09ff5fad66
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-01 17:26:09 +0300

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print all elements of a list
+ * @h: head node
+ * Return: number of elements in a list
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
