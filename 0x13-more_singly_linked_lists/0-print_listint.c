dda810a39eceaebd9f7b194cba3e0af8b818da60
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-01 18:16:25 +0300

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
