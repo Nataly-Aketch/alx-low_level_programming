eba6ac62d924140d8d2d802687c0b7302473090e
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-02 19:18:47 +0300

    Insert node in the middle

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (0);
 	new->n = n;
 	if (!*head)
-		*head = new;
+		return (0);
 	temp = *head;
 	while (count < idx)
 	{
