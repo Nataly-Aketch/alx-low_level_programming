9fdc4d4f243b0f0af857a348ff82c44e3b5ec12f
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-05 17:19:16 +0300

    Insert mode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,9 +38,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		*head = new;
 	}
-	else if (idx != 0)
+	else
 	{
 		prev = get_nodeint_at_index(*head, idx);
+		if (prev == NULL)
+			return (0);
 		new->next = prev->next;
 		prev->next = new;
 	}
