0944b79f349d7113684bc6743afad1fbc814b5f6
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-05 22:50:08 +0300

    Delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - searches a list
+ * @head: head node
+ * @index: node index
+ * Return: index of the node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head && count < index)
+	{
+		head = head->next;
+		count++;
+	}
+	return (head);
+}
+/**
+ * delete_nodeint_at_index - pops node at a specific index
+ * @head: head node
+ * @index: given index
+ * Return: 1 on success -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *popit, *prev;
+
+	if (*head && index == 0)
+	{
+		popit = get_nodeint_at_index(*head, index);
+		*head = popit->next;
+		free(popit);
+		return (1);
+	}
+	else if (*head && index != 0)
+	{
+		prev = get_nodeint_at_index(*head, index - 1);
+		popit = get_nodeint_at_index(*head, index);
+		prev->next = popit->next;
+		free(popit);
+		return (1);
+	}
+	return (-1);
+}
