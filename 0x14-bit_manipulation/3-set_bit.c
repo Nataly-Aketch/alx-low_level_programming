94dd5ebd818d059fc3fcd82fa85da9ee0e1ee392
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-07 09:53:39 +0300

    Set bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,6 +2,7 @@
 /**
  * set_bit - sets a value of a bit to 1
  * @n: array of int
+ * @index: index position
  * Return: 1 on success, -1 on failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
