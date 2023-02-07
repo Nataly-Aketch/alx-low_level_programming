78c80a12b85895e99108f7d09192c31515d6340b
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-07 09:52:34 +0300

    Set bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_bit - sets a value of a bit to 1
+ * @n: array of int
+ * Return: 1 on success, -1 on failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n |= 1 << index;
+	return (1);
+}
