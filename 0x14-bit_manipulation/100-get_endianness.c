3745a944b31e233c6ce057624f14140734348e14
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-07 21:23:08 +0300

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - checks endianness
+ * Return: 1 if little 0 if big
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (c)
+		return (1);
+	else
+		return (0);
+}
