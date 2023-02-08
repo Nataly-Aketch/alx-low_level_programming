67ca2c7150425da5350b5e1ce08c6cd0f1921bf4
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-09 00:38:42 +0300

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,24 +12,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (op == -1)
+	if (op == -1 || !len)
 	{
 		return (-1);
 	}
-	if (!text_content)
+	if (text_content)
 	{
-		close(op);
-		return (1);
-	}
-	if (!len)
-	{
-		return (-1);
-	}
-	pt = write(op, text_content, len);
-	if (pt == -1)
-	{
-		close(op);
-		return (-1);
+		pt = write(op, text_content, len);
+		if (pt == -1)
+			return (-1);
 	}
 	close(op);
 	return (1);
