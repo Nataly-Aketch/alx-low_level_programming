c397bb4b19053d0811846eebfb8548b47ebaba83
Nataly-Aketch <nataliebeckie@gmail.com>
2023-02-08 18:01:20 +0300

    Requiescat

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
+/**
  * read_textfile - reads textfile and prints it to stdout
  * @filename: name of file
- * @letter: length of text in file
+ * @letters: length of text in file
+ * Return: number of bytes written
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,5 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	wr = write(STDOUT_FILENO, s, strlen(s));
 	if (op == -1 || rd == -1 || wr == -1)
 		return (0);
+	close(op);
+	free(s);
 	return (wr);
 }
