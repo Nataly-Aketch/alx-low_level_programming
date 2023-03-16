4c2368882261bb239685e0ccef7ad9a6eee0c22d
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-16 16:21:18 +0300

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new)
 		return (0);
 	new->n = n;
-	if (*h == NULL)
+	if (*h == NULL || (*h)->prev == NULL)
 		*h = new;
 	else
 	{
