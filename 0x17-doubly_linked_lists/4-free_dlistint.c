8bae49c94b90d072b5241fcd2646c63669bf1630
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-16 14:11:39 +0300

    Free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a double linked list
+ * @head: head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
