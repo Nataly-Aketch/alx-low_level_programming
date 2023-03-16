be1e88806298bb1507fd265c3db1df70bf05b8d9
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-16 13:49:05 +0300

    Append node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -22,6 +22,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		last = *head;
 		while (last->next)
 			last = last->next;
+		new->prev = last;
 		last->next = new;
 	}
 	return (new);
