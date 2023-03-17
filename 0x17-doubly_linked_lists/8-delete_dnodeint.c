645af3b3754f3995d9ad17c372372d82fb5b55d6
Nataly-Aketch <nataliebeckie@gmail.com>
2023-03-17 09:22:41 +0300

    Popit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0 && *head)
 	{
-		popit = *head;
+		popit = get_dnodeint_at_index(*head, index);
 		if (popit == NULL)
 			return (-1);
 		*head = popit->next;
