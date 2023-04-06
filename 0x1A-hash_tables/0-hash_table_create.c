b6cc6b32949e3cb5ece1fe6028e0c05c17d6a864
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-06 18:10:26 +0300

    Readme

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 	unsigned long int i;
-	hash_node_t **items;
 
 	if (!new_table)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(*items) * new_table->size);
+	new_table->array = malloc(sizeof(hash_node_t *) * new_table->size);
 	if (!new_table->array)
 		return (NULL);
 	for (i = 0; i < new_table->size; i++)
