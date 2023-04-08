c169f6be1d7e9b5f6991e28ca05d973179a30ded
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-08 16:47:40 +0300

    Add item

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,10 +10,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *head;
 
-	if (!new_node)
+	if (!new_node || !ht)
 		return (0);
 	hash = key_index((unsigned char *)key, ht->size);
+	head = ht->array[hash];
+	while (head)
+	{
+		if (strcmp(head->key, key) != 0)
+		{
+			head->value = strdup(value);
+			if (!value)
+				return (0);
+			return (1);
+		}
+		head = head->next;
+	}
 	new_node->value = strdup(value);
 	if (!value)
 	{
