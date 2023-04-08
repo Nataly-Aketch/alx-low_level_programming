6f0e1358048e47b9b4bd01cd794cf5a794a7d436
Nataly-Aketch <nataliebeckie@gmail.com>
2023-04-08 16:49:18 +0300

    Add item

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	head = ht->array[hash];
 	while (head)
 	{
-		if (strcmp(head->key, key) != 0)
+		if (strcmp(head->key, key) == 0)
 		{
 			head->value = strdup(value);
 			if (!value)
